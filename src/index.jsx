f55fe13445b9a5a8280372c9aa950dc88f2d5144
sottar <ohrst.18@gmail.com>
2017-04-27 00:26:25 +0900

    init react app

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,17 @@
+/* @flow */
+
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>app</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);

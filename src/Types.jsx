3082583c72683ec92e87c7fa7d84b9b21d8b7bb6
sottar <ohrst.18@gmail.com>
2017-05-01 15:18:52 +0900

    update level1 and comments

--- src/Types.jsx
+++ src/Types.jsx
@@ -2,21 +2,21 @@
 
 export type BoxInfo = {
   id: number;
-  position: Array<number>;
+  position: Array<number>; // [Horizontal, Vertical]. [0, 0] is upper left position
   color: string;
-  cleared: boolean;
+  cleared: boolean; // true is matched goal's position and color
 };
 
 export type GoalInfo = {
   id: number;
-  position: Array<number>;
+  position: Array<number>; // [Horizontal, Vertical]. [0, 0] is upper left position
   color: string;
-  cleared: boolean;
+  cleared: boolean; // true is matched box's position and color
 };
 
 export type FieldInfo = {
-  matrix: Array<number>;
-  availablePositions: Array<Array<number>>;
+  matrix: Array<number>; // [Horizontal, Vertical]
+  availablePositions: Array<Array<number>>; // 1 is enable to moving
   boxInfo: Array<BoxInfo>;
   goalInfo: Array<GoalInfo>;
 }
